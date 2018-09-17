5bed1f284f0efb63f5ee96102a92de572a981c48
MadeYoga <vngla21@gmail.com>
2018-09-17 17:47:09 +0700

    adds new circle methods

--- MatrixVector/Bezier/Source.cpp
+++ MatrixVector/Bezier/Source.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 vector <Vector> points;
 
+vector <Vector> cloud_vector[10];
+
 float factorial(int n) {
 	if (n <= 1)
 		return(1);
@@ -58,9 +60,9 @@ void DrawTriangle(Vector v1, Vector v2, Vector v3) {
 }
 
 void DrawHalfCircle(Vector point, int radius) {
-	glPointSize(0.5); //ukuran titik/point
+	glPointSize(1.5); //ukuran titik/point
 
-	for (float i = -1.57; i < 1.57; i += 0.0001)
+	for (float i = 1; i < 5; i += 0.0001)
 	{
 		float x = point.x + cos(i) * radius;
 		float y = point.y + sin(i) * radius;
@@ -72,17 +74,28 @@ void DrawHalfCircle(Vector point, int radius) {
 }
 
 void DrawCircle(Vector point, int radius) {
+	glBegin(GL_POINTS);
 	glPointSize(0.5); //ukuran titik/point
 
 	for (float i = -3.14; i < 3.14; i += 0.0001)
 	{
 		float x = point.x + cos(i) * radius;
 		float y = point.y + sin(i) * radius;
-
-		glBegin(GL_POINTS);
 		glVertex3f(x, y, 0);
-		glEnd();
 	}
+	glEnd();
+}
+
+void DrawCircle(Vector point, float r_x, float r_y)
+{
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(point.x, point.y);
+	for (int i = 0; i <= 360; i++) {
+		float x = point.x + cos(i) * r_x;
+		float y = point.y + sin(i) * r_y;
+		glVertex2f(x, y);
+	}
+	glEnd();
 }
 
 void DrawKotak() {
@@ -114,6 +127,27 @@ void DrawBezier(vector<Vector> v) {
 	glEnd();
 }
 
+void DrawBezier(vector<Vector> v[]) {
+	glBegin(GL_POINTS);
+	for (int i = 0; i < 6; i++) {
+		for (float t = 0; t <= 1; t += 0.0001) {
+			Vector calculated_v = get_calculated_bezier_point(v[i], t);
+			glVertex3f(calculated_v.x, calculated_v.y, calculated_v.z);
+		}
+	}
+	glEnd();
+}
+
+void drawCloud(const Vector pos) {
+	int x = pos.x, y = pos.y;
+
+	DrawCircle(Vector(x, y, 0, 1), 80, 45);
+	DrawCircle(Vector(x + 25, y + 15, 0, 1), 35, 35);
+	DrawCircle(Vector(x + 25, y - 15, 0, 1), 35, 35);
+	DrawCircle(Vector(x - 25, y - 15, 0, 1), 35, 35);
+	DrawCircle(Vector(x - 25, y + 15, 0, 1), 35, 35);
+}
+
 void DisplayHouse() {
 	///// CLEAR /////
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -124,7 +158,7 @@ void DisplayHouse() {
 	///// DRAW /////
 
 	// CRESCENT MOON
-	Vector c1(-552, 750, 0, 1), c3(-552, 352, 0, 1), c2(-350, 525, 0, 1);
+	Vector c1(-442, 718, 0, 1), c3(-496, 360, 0, 1), c2(-688, 584, 0, 1);
 	vector<Vector> v;
 	v.push_back(c1);
 	v.push_back(c2);
@@ -132,6 +166,11 @@ void DisplayHouse() {
 	DrawHalfCircle(Vector(-550, 550, 0, 1), 200); // DRAW 1/2 CIRCLE
 	DrawBezier(v);
 	
+
+	// Cloud
+	// DrawBezier(cloud_vector);
+	drawCloud(Vector(500, 600, 0, 1));
+	drawCloud(Vector(400, 600, 0, 1));
 
 	// LINES
 	glColor3f(0.1, 0.1, 1);
@@ -197,6 +236,7 @@ void OnMouseClick(int button, int state, int x, int y) {
 		float ox = x - WINDOW_SIZE / 2;
 		float oy = y - WINDOW_SIZE / 2;
 		points.push_back(Vector(ox * 2, -oy * 2, 0, 1));
+		cout << ox * 2 << " : " << -oy * 2 << endl;
 	}
 
 	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
@@ -205,6 +245,41 @@ void OnMouseClick(int button, int state, int x, int y) {
 }
 
 void main(int argc, char **argv) {
+
+	// OLD CLOUD USING BEZIER
+	// CLOUD VECTOR VALUES
+	/*Vector v1 (252, 644, 0, 1), v2 (380, 724, 0, 1), v3 (496, 648, 0, 1),
+		   v4 (496, 648, 0, 1), v5 (578, 684, 0, 1), v6 (642, 640, 0, 1),
+		   v7 (252, 646, 0, 1), v8 (128, 624, 0, 1), v9 (148, 532, 0, 1),
+		   v10(644, 638, 0, 1), v11(706, 572, 0, 1), v12(604, 512, 0, 1),
+		   v13(148, 530, 0, 1), v14(150, 476, 0, 1), v15(230, 484, 0, 1), v16(248, 507, 0, 1);
+
+	cloud_vector[0].push_back(v1);
+	cloud_vector[0].push_back(v2);
+	cloud_vector[0].push_back(v3);
+
+	cloud_vector[1].push_back(v4);
+	cloud_vector[1].push_back(v5);
+	cloud_vector[1].push_back(v6);
+
+	cloud_vector[2].push_back(v7);
+	cloud_vector[2].push_back(v8);
+	cloud_vector[2].push_back(v9);
+
+	cloud_vector[3].push_back(v10);
+	cloud_vector[3].push_back(v11);
+	cloud_vector[3].push_back(v12);
+
+	cloud_vector[4].push_back(v13);
+	cloud_vector[4].push_back(v14);
+	cloud_vector[4].push_back(v15);
+	cloud_vector[4].push_back(v16);
+	
+	cloud_vector[5].push_back(Vector(248, 508, 0, 1));
+	cloud_vector[5].push_back(Vector(300, 362, 0, 1));
+	cloud_vector[5].push_back(Vector(370, 612, 0, 1));
+	cloud_vector[5].push_back(Vector(466, 350, 0, 1));
+	cloud_vector[5].push_back(Vector(602, 514, 0, 1));*/
 
 	// INIT
 	glutInit(&argc, argv);
