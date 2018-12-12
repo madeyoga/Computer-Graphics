dd1b977097e4d01a977422902830eff821ec3bbb
Yeogaa <vngla21@gmail.com>
2018-12-12 23:39:57 +0700

    Update Face.h

--- Classes/Entities/Face.h
+++ Classes/Entities/Face.h
@@ -3,23 +3,19 @@
 class Face 
 {
 public:
-	
 	vector<int> vertex_indices, normal_indices, uv_indices;
 	int *v, *vt, *vn;
 	int length;
-
 	Face() 
 	{
 
 	}
-
 	Face(vector<int> &v1, vector<int> &v2, vector<int> &v3) 
 	{
 		vertex_indices = v1;
 		normal_indices = v2;
 		uv_indices = v3;
 	}
-
 	Face(int n) 
 	{
 		length = n;
@@ -27,7 +23,6 @@ public:
 		vt = new int[length];
 		vn = new int[length];
 	}
-
 	void LoadFace(int arr[], int arr2[], int arr3[]) 
 	{
 		for (int i = 0; i < length; i++)
